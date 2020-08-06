ef2b60d8d9fa52d43f7fa1f7e8f278ab2c5a5aa0
tnb1021 <54203789+tnb1021@users.noreply.github.com>
2020-08-06 18:05:07 +0900

    Update 01.py

--- part1/01.py
+++ part1/01.py
@@ -3,6 +3,8 @@ _str = "パタトクカシーー"
 _list = [_str[0],_str[2],_str[4],_str[6]] #1つ1つ取り出しリストへ格納
 ans_1 = ''.join(_list)
 
-ans_2 = _str[::2] #start: stop: step2
+ans_2 = _str[0]+_str[2]+_str[4]+_str[6]
 
-print(ans_1,ans_2) #パトカー パトカー
+ans_3 = _str[::2] #start: stop: step2
+
+print(ans_1,ans_2,ans_3) #パトカー パトカー パトカー
