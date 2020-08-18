e2c7696773680fab5b66177b0e69dcdd7feac3fa
tnb1021 <54203789+tnb1021@users.noreply.github.com>
2020-08-18 19:18:37 +0900

    Add files via upload

---
+++ part2/13.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+df1 = pd.read_csv('col1.txt',header=None,delimiter='\t')
+df2 = pd.read_csv('col2.txt',header=None,delimiter='\t')
+
+df = pd.concat([df1,df2],axis=1)
+
+df.to_csv('merge.txt',sep='\t',header=False,index=False)
