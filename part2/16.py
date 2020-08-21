eb1bb3b103b43773ed8353bc3202bd0d840958ee
tnb1021 <54203789+tnb1021@users.noreply.github.com>
2020-08-21 17:11:00 +0900

    Update 16.py

--- part2/16.py
+++ part2/16.py
@@ -5,7 +5,9 @@ args = sys.argv
 n = int(args[1])
 df = pd.read_csv('data.txt',header=None,delimiter='\t')
 
-target = [i+i*df.shape[0]//n+1 for i in range(n)]
+target = [i+i*df.shape[0]//n for i in range(n)]
 result = [df[j:j+df.shape[0]//n+1] for j in target]
 
 print(result)
+print(target)
+print(df.shape[0])
